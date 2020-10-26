b7d03342e8af009e410101b1c799f9ebcf98f58d
jds-walker <jdsw86@gmail.com>
2020-10-26 14:52:51 +1100

    select

--- main.py
+++ main.py
@@ -5,24 +5,20 @@ from tiger import TigerProblem
 if __name__ == "__main__":
     
     tree = Node("root")
-
     tiger = TigerProblem()
 
     tree.add_children(tiger.all_actions())
+    tree.select(tiger)
 
 
-    tiger.set_action(tree.select().data)
-    print(tiger.action)
-    tiger.get_observation()
-    print(tiger.observation)
     
-    print("\n\nRun simulation")
-    while tiger.terminal == False:
-        tiger.get_action()
-        print(tiger.action)
-        tiger.get_observation()
-        print(tiger.observation)
-        tiger.get_reward()
-        print(tiger.reward)
+    # print("\n\nRun simulation")
+    # while tiger.terminal == False:
+    #     tiger.get_action()
+    #     print(tiger.action)
+    #     tiger.get_observation()
+    #     print(tiger.observation)
+    #     tiger.get_reward()
+    #     print(tiger.reward)
 
-    print(tiger.all_actions())
+    # print(tiger.all_actions())
--- node.py
+++ node.py
@@ -1,7 +1,10 @@
 from random import choice
 from enum import Enum
+from tiger import TigerProblem
 
 class Node:
+
+
 
     def __init__(self, data, tier=1):
         self.data = data
@@ -10,18 +13,32 @@ class Node:
         self.children = dict()
         self.tier = tier # 1 . Action 0 . Observation
 
-    def select(self):
-        if len(self.children) == 0:
-            return self
-        else:
-            max_key = max(self.children, key=lambda n:  self.children[n].V)
-            max_value = self.children[max_key].V
-            max_keys = [k for k, v in self.children.items() if v.V == max_value]            
-            return self.children[choice(max_keys)].select()
+    def select(self, problem: TigerProblem):
+        max_key = max(self.children, key=lambda n:  self.children[n].V)
+        max_value = self.children[max_key].V
+        max_keys = [k for k, v in self.children.items() if v.V == max_value]            
+        selected = self.children[choice(max_keys)]
+        if len(selected.children) == 0:
+            #rolllout
+            a = 1+1
+            print(a)
+        else: 
+            print("act")
+            selected.act(problem)    
 
+    def act(self, problem: TigerProblem):
+        problem.set_action(self.data)
+        observation = problem.get_observation()
+        if observation in self.children:
+            self.children[observation].select()
+        else:
+            self.children[observation] = Node(observation, tier=0) # Obtain first observation
+            self.children[observation].add_children(problem.all_actions())
+        
     def add_children(self, children):
         for child in children:
             self.children[child] = Node(child)
+        
 
     def print_tree(self):
         print(self.data)
